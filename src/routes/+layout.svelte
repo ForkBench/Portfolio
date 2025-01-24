060ef1accbe8ae0dce4ebf889ad513731a3cf066
Forkbench <forkbench@proton.me>
2025-01-24 13:46:21 +0100

    Adding navbar, and flowbite

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,25 @@
 <script lang="ts">
 	import '../app.css';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<Navbar rounded color="red" class="w-4/5 m-auto mt-4">
+	<NavBrand href="/">
+		<img src="/Inseki.png" class="me-3 h-6 sm:h-9" alt="Logo" />
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Forkbench</span>
+	</NavBrand>
+	<NavHamburger />
+	<NavUl>
+		<NavLi href="/">Home</NavLi>
+		<NavLi href="/projects">Projects</NavLi>
+		<NavLi href="/about">About</NavLi>
+		<NavLi href="/contact">Contact</NavLi>
+	</NavUl>
+</Navbar>
+
+<main class="w-4/5 m-auto">
+	{@render children()}
+</main>
+
